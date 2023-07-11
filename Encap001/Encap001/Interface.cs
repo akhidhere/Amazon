9c9147c2d19b1576dedee9e779b5a11d68a12fdc
akhidhere <139128631+akhidhere@users.noreply.github.com>
2023-07-11 13:07:15 +0530

    Commit changes

---
+++ Encap001/Encap001/Interface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encap001
+{
+    public interface A
+    {
+        void m1();
+    }
+
+    class B:A
+
+    {
+        public void m1()
+        {
+            Console.WriteLine("mi is a interface ");
+        
+        }
+
+        public static void Main(string[] args)
+
+        {
+
+
+            B a = new B();
+
+            a.m1();
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Encap001/Encap001/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Account
+    {
+
+        int myaccountbalance = 1000;
+
+        public void setbalance(int amount)
+        {
+            myaccountbalance = amount;
+
+        }
+        public void getbalance()
+        {
+
+            Console.WriteLine("your acco balance is:" + myaccountbalance);
+        }
+
+        class balance
+        {
+
+            public static void Main(string[] args)
+            {
+
+                Account myAccount = new Account();
+                myAccount.setbalance(10000);
+                myAccount.getbalance();
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
